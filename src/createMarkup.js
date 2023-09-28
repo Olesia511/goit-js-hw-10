983b320aedbab252f19ddee2a0fd0f501247512f
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-09-29 00:09:29 +0300

    HW-10/1

--- src/createMarkup.js
+++ src/createMarkup.js
@@ -15,10 +15,11 @@ export function createCard(cat) {
   const { name, origin, description, temperament } = cat.breeds[0];
 
   const catCard = `<img src="${url}" alt="${name}" />
+        <div class="cat-temperament">
         <h2>${name}</h2>
         <h4>Country: ${origin}</h4>
         <h5>Temperament: ${temperament}</h5>
-        <h5>${description}</h5>`;
+        <h5>${description}</h5></div>`;
 
   catInfo.innerHTML = catCard;
 }
